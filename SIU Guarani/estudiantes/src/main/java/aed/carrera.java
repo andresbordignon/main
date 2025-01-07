6ab385a1ba0a8a966c9bb5efe8140abb8dac2db4
andresbordignon <142251815+andresbordignon@users.noreply.github.com>
2025-01-07 00:20:39 -0300

    Agrego codigos del trabajo práctico de la materia AED2

---
+++ SIU Guarani/estudiantes/src/main/java/aed/carrera.java
@@ -0,0 +1,13 @@
+package aed;
+
+import java.util.*;
+
+public class carrera {
+    private Trie<Materia> trieMaterias;
+
+    public carrera(){
+        this.trieMaterias = new Trie<Materia>();
+    }
+
+}
+
